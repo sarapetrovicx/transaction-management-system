90db055d4f3e40f26425bbdafbb17d51c775874c
Sara Petrovic <sara_petrovic@homedepot.com>
2026-03-28 20:35:35 +0100

    ng generate components

---
+++ frontend/src/app/components/add-transaction-modal/add-transaction-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-transaction-modal',
+  imports: [],
+  templateUrl: './add-transaction-modal.html',
+  styleUrl: './add-transaction-modal.css',
+})
+export class AddTransactionModal {}
---
+++ frontend/src/app/components/transaction-table/transaction-table.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-table',
+  imports: [],
+  templateUrl: './transaction-table.html',
+  styleUrl: './transaction-table.css',
+})
+export class TransactionTable {}
