01d53763ee11054c96e61f24c87f52f004fe7dab
Sara Petrovic <sara_petrovic@homedepot.com>
2026-03-29 17:16:31 +0200

    import bootstrap

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
+/// <reference types="@angular/localize" />
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/core/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
