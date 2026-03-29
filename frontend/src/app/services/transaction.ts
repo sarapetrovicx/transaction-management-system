75f06b170be54d37196e2bbe40f499e7b56cb5d2
Sara Petrovic <sara_petrovic@homedepot.com>
2026-03-29 16:18:29 +0200

    add service logic

--- frontend/src/app/services/transaction.ts
+++ frontend/src/app/services/transaction.ts
@@ -1,6 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { Transaction, TransactionRequest } from '../models/transaction'
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Transaction {}
+
+export class TransactionService {
+
+  private apiUrl = 'http://localhost:8080/api/transactions'
+
+  constructor(private http: HttpClient) {}
+
+  getTransactions(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.apiUrl)
+  }
+
+  addTransaction(transaction: TransactionRequest): Observable<any> {
+    return this.http.post(this.apiUrl, transaction)
+  }
+}
