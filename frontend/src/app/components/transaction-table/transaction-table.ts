9d6e1c8a9c308a98b397069b5a19d041681a6219
Sara Petrovic <sara_petrovic@homedepot.com>
2026-03-29 16:20:28 +0200

    add basic table

--- frontend/src/app/components/transaction-table/transaction-table.ts
+++ frontend/src/app/components/transaction-table/transaction-table.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { TransactionService } from '../../services/transaction'
+import { Transaction } from '../../models/transaction'
+import { AddTransactionModalComponent } from '../add-transaction-modal/add-transaction-modal'
 
 @Component({
   selector: 'app-transaction-table',
-  imports: [],
-  templateUrl: './transaction-table.html',
-  styleUrl: './transaction-table.css',
+  standalone: true,
+  imports: [CommonModule, AddTransactionModalComponent],
+  templateUrl: './transaction-table.html'
 })
-export class TransactionTable {}
+export class TransactionTableComponent implements OnInit {
+
+  transactions: Transaction[] = []
+
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.loadTransactions()
+  }
+
+  loadTransactions() {
+    this.transactionService.getTransactions()
+      .subscribe(data => this.transactions = data)
+  }
+}
